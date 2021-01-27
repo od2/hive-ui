3300e03d1ef22702783327fd27ddf9ecd28bdcfb
Richard Patel <me@terorie.dev>
2021-01-28 00:24:03 +0100

    Switch back to npm
    GitHub Package Registry sucks

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,7 @@ import { createStore } from "vuex";
 import { CreateWorkerTokenRequest } from "@od2/hive-api/management_pb";
 import { ManagementPromiseClient } from "@od2/hive-api/management_grpc_web_pb";
 
-const managementClient = new ManagementPromiseClient(
-  "https://hive.od2.network/grpc"
-);
+const managementClient = new ManagementPromiseClient("/grpc");
 
 export default createStore({
   state: {},
