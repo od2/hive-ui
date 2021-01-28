1b854b4e4a7578e10e8d7e58bf59c088b645bf33
Richard Patel <me@terorie.dev>
2021-01-28 05:32:09 +0100

    Progress Tokens view

--- src/views/Tokens.vue
+++ src/views/Tokens.vue
@@ -1,21 +1,11 @@
 <template>
-  <div class="columns">
-    <div class="column is-three-quarters">
-      <h1 class="title">Tokens</h1>
-      <h2 class="subtitle">Auth tokens for Hive CLI tooling</h2>
-    </div>
-    <div class="column buttons">
-      <button class="button is-primary">New</button>
-      <button class="button is-danger is-outlined">Revoke All</button>
-    </div>
-  </div>
+  <h1 class="title is-1">Tokens</h1>
   <p>
-    Hive CLI applications need tokens to run. Currently, they allow full access
-    to the worker API.
-    <br />
-    They can only be seen on creation, so save them somewhere secure.
+    Hive CLI applications need tokens to run. Tokens grant full access to the
+    worker API.
   </p>
   <hr />
+  <h3 class="title is-4">Active Tokens</h3>
   <table class="table" id="od2-tokens-list">
     <thead>
       <tr>
@@ -23,6 +13,11 @@
         <th>Token</th>
         <th>Created</th>
         <th>Last used</th>
+        <th>
+          <button class="button is-fullwidth is-small is-danger is-outlined">
+            Revoke All
+          </button>
+        </th>
       </tr>
     </thead>
     <tbody>
@@ -32,7 +27,7 @@
         <td class="od2-col-shrink">2020-11-11 11:11</td>
         <td class="od2-col-shrink">2021-01-23 03:50</td>
         <td class="od2-col-shrink">
-          <button class="button is-small is-danger is-outlined">Revoke</button>
+          <button class="button is-fullwidth is-small is-danger">Revoke</button>
         </td>
       </tr>
       <tr>
@@ -41,14 +36,47 @@
         <td class="od2-col-shrink">2020-11-11 11:12</td>
         <td class="od2-col-shrink">2021-01-23 03:50</td>
         <td class="od2-col-shrink">
-          <button class="button is-small is-danger is-outlined">Revoke</button>
+          <button class="button is-fullwidth is-small is-danger">Revoke</button>
         </td>
       </tr>
     </tbody>
   </table>
+  <hr />
+  <h3 class="title is-4">Create New Token</h3>
+  <div class="columns mt-4">
+    <div id="newTokenNameField" class="column field">
+      <label class="label">Description</label>
+      <div class="control">
+        <input class="input" type="text" />
+      </div>
+      <p class="help">What's this token for?</p>
+    </div>
+    <div class="column is-narrow field">
+      <div class="control">
+        <button id="newTokenSubmit" class="button is-primary">Create</button>
+      </div>
+    </div>
+  </div>
+  <div class="notification is-primary is-light">
+    <strong>Your Token:&ensp;</strong>
+    <tt>HCzSGK1WSf4OlRQzcxk0uFwYo5riFOMI-ryAhSv6IlHdP</tt>
+    <p class="mt-2">
+      Be sure to save this token. You won't be able to see it again.
+    </p>
+  </div>
 </template>
 
 <style scoped>
+#newTokenNameField {
+  max-width: 30em;
+}
+
+@media (min-width: 48em) {
+  #newTokenSubmit {
+    margin-top: 24pt;
+  }
+}
+
 .od2-col-shrink {
   white-space: nowrap;
 }
