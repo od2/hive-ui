0d7520ca45d4f2993433800172d7faa6e0770af8
Richard Patel <me@terorie.dev>
2021-01-28 09:08:13 +0100

    Remove navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,4 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <router-link to="/" class="navbar-item">Home</router-link>
-      </div>
-      <div class="navbar-end">
-        <a class="navbar-item">Login</a>
-      </div>
-    </div>
-  </nav>
   <main class="section">
     <div class="container">
       <nav class="breadcrumb" aria-label="breadcrumbs">
